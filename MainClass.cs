03b5fd69a0da8210655245828a5d2fb8ea3ca8db
Sulimov Vladimir <sulimov666@mail.ru>
2021-01-11 16:23:06 +0400

    Настройка Git

---
+++ MainClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Payroll
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            Console.Write("Представьтесь, пожалуйста:");
+            string user = Console.ReadLine();
+        }
+    }
+}
